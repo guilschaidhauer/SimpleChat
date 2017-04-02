c9c89b3327277e97653d32248f16e81bc3833617
guilschaidhauer <e.guilherme.castro@gmail.com>
2017-04-02 20:59:36 -0300

    Simple chat working

--- SimpleChat/SimpleChat/SimpleChat.cpp
+++ SimpleChat/SimpleChat/SimpleChat.cpp
@@ -31,17 +31,40 @@ int main()
 		listener.listen(2000);
 		listener.accept(socket);
 		text += "Server";
+		mode = 's';
 	}
 	else if (connectionType == 'c')
 	{
 		socket.connect(ip, 2000);
 		text += "Client";
+		mode = 'r';
 	}
 
 	socket.send(text.c_str(), text.size() + 1);
 	socket.receive(buffer, sizeof(buffer), received);
 
 	cout << buffer << endl;
+
+	bool done = false;
+
+	while (!done)
+	{
+		if (mode == 's')
+		{
+			getline(cin, text);
+			socket.send(text.c_str(), text.size() + 1);
+			mode = 'r';
+		}
+		else if (mode == 'r')
+		{
+			socket.receive(buffer, sizeof(buffer), received);
+			if (received > 0)
+			{
+				cout << "Received: " << buffer << endl;
+				mode = 's';
+			}
+		}
+	}
 
 	system("pause");
 	return 0;
