3090a75c017464e4da9d79ce7f100c69a8a98902
guilschaidhauer <e.guilherme.castro@gmail.com>
2017-04-03 23:11:41 -0300

    Add chat between two clients

--- SimpleChat/SimpleChat/SimpleChat.cpp
+++ SimpleChat/SimpleChat/SimpleChat.cpp
@@ -1,8 +1,3 @@
-// Anything after // is a comment not actual C++ code
-// Comments are important and I use them to explain things
-// Why not read the comments in this code
-
-// These "include" code from the C++ library and SFML too
 #include "stdafx.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
@@ -11,8 +6,6 @@
 
 using namespace std;
 
-// This is the main C++ program- Duh!
-// It is where our game starts from
 int main()
 {
 	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
@@ -46,7 +39,8 @@ int main()
 
 		text1 += "Server";
 		text2 += "Server";
-		mode = 's';
+
+		mode = 'r';
 
 		socket1.send(text1.c_str(), text1.size() + 1);
 		socket1.receive(buffer1, sizeof(buffer1), received1);
@@ -61,7 +55,7 @@ int main()
 	{
 		socket1.connect(ip, 2000);
 		text1 += "Client1";
-		mode = 'r';
+		mode = 's';
 
 		socket1.send(text1.c_str(), text1.size() + 1);
 		socket1.receive(buffer1, sizeof(buffer1), received1);
@@ -88,25 +82,63 @@ int main()
 	{
 		if (connectionType == 's')
 		{
-			socket1.receive(buffer1, sizeof(buffer1), received1);
-			if (received1 > 0)
+			if (mode == 'r')
 			{
-				cout << "Received: " << buffer1 << endl;
-				socket2.send(buffer1, sizeof(buffer1));
+				socket1.receive(buffer1, sizeof(buffer1), received1);
+				if (received1 > 0)
+				{
+					cout << "Received: " << buffer1 << endl;
+					socket2.send(buffer1, sizeof(buffer1));
+					mode = 's';
+				}
+			}
+			else if (mode == 's')
+			{
+				socket2.receive(buffer2, sizeof(buffer2), received2);
+				if (received2 > 0)
+				{
+					cout << "Received: " << buffer2 << endl;
+					socket1.send(buffer2, sizeof(buffer2));
+					mode = 'r';
+				}
 			}
 		}
 		else if (connectionType == '1')
 		{
-			cout << "Type in your message: ";
-			cin >> text1;
-			socket1.send(text1.c_str(), text1.size() + 1);
+			if (mode == 's') 
+			{
+				cout << "Type in your message: ";
+				cin >> text1;
+				socket1.send(text1.c_str(), text1.size() + 1);
+				mode = 'r';
+			}
+			else if (mode == 'r')
+			{
+				socket1.receive(buffer1, sizeof(buffer1), received1);
+				if (received1 > 0)
+				{
+					cout << "Received from client 2: " << buffer1 << endl;
+					mode = 's';
+				}
+			}
 		}
 		else if (connectionType == '2')
 		{
-			socket2.receive(buffer2, sizeof(buffer2), received2);
-			if (received2 > 0)
+			if (mode == 's')
 			{
-				cout << "Received from client 1: " << buffer2 << endl;
+				cout << "Type in your message: ";
+				cin >> text2;
+				socket2.send(text2.c_str(), text2.size() + 1);
+				mode = 'r';
+			}
+			else if (mode == 'r')
+			{
+				socket2.receive(buffer2, sizeof(buffer2), received2);
+				if (received2 > 0)
+				{
+					cout << "Received from client 1: " << buffer2 << endl;
+					mode = 's';
+				}
 			}
 		}
 
