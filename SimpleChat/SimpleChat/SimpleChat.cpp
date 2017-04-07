7285b9e40e5fac56173517ef096d084a97c629aa
guilschaidhauer <e.guilherme.castro@gmail.com>
2017-04-07 09:50:25 -0300

    Work

--- SimpleChat/SimpleChat/SimpleChat.cpp
+++ SimpleChat/SimpleChat/SimpleChat.cpp
@@ -3,6 +3,8 @@
 #include <SFML/Network.hpp>
 #include <iostream>
 #include <string>
+#include <Windows.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -11,7 +13,9 @@ int main()
 	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
 
 	sf::TcpSocket socket1;
+	//socket1.setBlocking(false);
 	sf::TcpSocket socket2;
+	//socket2.setBlocking(false);
 	
 	char buffer1[2000];
 	char buffer2[2000];
@@ -19,15 +23,15 @@ int main()
 	size_t received1;
 	size_t received2;
 
-	char connectionType, mode;
+	string connectionType, mode;
 
 	string text1 = "Connected to ";
 	string text2 = "Connected to ";
 
-	cout << "Enter (s) for Server, Enter for (c) for client" << endl;
-	cin >> connectionType;
+	cout << "Enter 's' to connect as a server, Enter for '1' for client 1 and '2' for client 2" << endl;
+	getline(cin, connectionType);
 
-	if (connectionType == 's')
+	if (connectionType == "s")
 	{
 		sf::TcpListener listener;
 		listener.listen(2000);
@@ -51,7 +55,7 @@ int main()
 		cout << buffer1 << endl;
 		cout << buffer2 << endl;
 	}
-	else if (connectionType == '1')
+	else if (connectionType == "1")
 	{
 		socket1.connect(ip, 2000);
 		text1 += "Client1";
@@ -62,7 +66,7 @@ int main()
 
 		cout << buffer1 << endl;
 	}
-	else if (connectionType == '2')
+	else if (connectionType == "2")
 	{
 		socket2.connect(ip, 2001);
 		text1 += "Client2";
@@ -76,23 +80,26 @@ int main()
 
 	cout << endl;
 
+	system("cls");
+	Sleep(1000);
+
 	bool done = false;
 
 	while (!done)
 	{
-		if (connectionType == 's')
+		if (connectionType == "s")
 		{
-			if (mode == 'r')
+			if (mode == "r")
 			{
 				socket1.receive(buffer1, sizeof(buffer1), received1);
 				if (received1 > 0)
 				{
 					cout << "Received: " << buffer1 << endl;
 					socket2.send(buffer1, sizeof(buffer1));
-					mode = 's';
+					mode = "s";
 				}
 			}
-			else if (mode == 's')
+			else if (mode == "s")
 			{
 				socket2.receive(buffer2, sizeof(buffer2), received2);
 				if (received2 > 0)
@@ -103,41 +110,49 @@ int main()
 				}
 			}
 		}
-		else if (connectionType == '1')
+		else if (connectionType == "1")
 		{
-			if (mode == 's') 
+			if (mode == "s") 
 			{
 				cout << "Type in your message: ";
-				cin >> text1;
+				getline(cin, text1);
+
+				if (text1.size() <= 0)
+					continue;
+
 				socket1.send(text1.c_str(), text1.size() + 1);
-				mode = 'r';
+				mode = "r";
 			}
-			else if (mode == 'r')
+			else if (mode == "r")
 			{
 				socket1.receive(buffer1, sizeof(buffer1), received1);
 				if (received1 > 0)
 				{
 					cout << "Received from client 2: " << buffer1 << endl;
-					mode = 's';
+					mode = "s";
 				}
 			}
 		}
-		else if (connectionType == '2')
+		else if (connectionType == "2")
 		{
-			if (mode == 's')
+			if (mode == "s")
 			{
 				cout << "Type in your message: ";
-				cin >> text2;
+				getline(cin, text2);
+
+				if (text2.size() <= 0)
+					continue;
+
 				socket2.send(text2.c_str(), text2.size() + 1);
-				mode = 'r';
+				mode = "r";
 			}
-			else if (mode == 'r')
+			else if (mode == "r")
 			{
 				socket2.receive(buffer2, sizeof(buffer2), received2);
 				if (received2 > 0)
 				{
 					cout << "Received from client 1: " << buffer2 << endl;
-					mode = 's';
+					mode = "s";
 				}
 			}
 		}
